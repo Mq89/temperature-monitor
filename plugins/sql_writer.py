2b7b2f638ae51152cb16d69e521af89e2ed1ca39
Markus Heinrich <mh@0x25.net>
2018-06-21 21:38:15 +0200

    commented sql plugin

--- plugins/sql_writer.py
+++ plugins/sql_writer.py
@@ -16,15 +16,31 @@ class SQLWriter():
         self._tab = "data"
 
     def update(self, mod, tin, tout):
+        """
+        @brief      Receive updates of temperature values
+        
+        @param      self  The object
+        @param      mod   The modifier
+        @param      tin   The inside temperature
+        @param      tout  The outside temperature
+        
+        @return     None
+        """
+
+        # Insert query
         query = "INSERT INTO {0} (sensor_id, value) VALUES ({1}, {2})"
 
         try:
             connection = MySQLdb.connect(self._server, self._user, "", self._db)
             cursor = connection.cursor()
+
+            # perform plausibility check on inside temperature and write into database
             if tin > t.Temperature.MIN_VALUE and tin < 100:
                 cursor.execute(query.format(self._tab, 1, tin))
             else:
                 self._log.error("tin out of range: {0}".format(tin))
+
+            # perform plausibility check on outside temperature and write into database
             if tout > t.Temperature.MIN_VALUE and tout < 100:
                 cursor.execute(query.format(self._tab, 2, tout))
             else:
@@ -35,6 +51,13 @@ class SQLWriter():
             self._log.error("Connection Error: Values {0}, {1}".format(tin, tout))
 
     def stop(self):
+        """
+        @brief      Stop the plugin
+        
+        @param      self  The object
+        
+        @return     None
+        """
         self._log.info("{0} stopped".format(self))
 
     def __str__(self):
