830398994d2971ee425bbabeeb3c4f0419d01359
Markus Heinrich <mh@0x25.net>
2018-06-21 21:26:31 +0200

    commented display plugin

--- plugins/display.py
+++ plugins/display.py
@@ -8,8 +8,16 @@ import logging
 
 class SegPlugin:
 
+    # hours of day where the LED are allowed to be active
+    _HOURS = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+
+    # GPIO pins for the 7 segmets
     _SEGMENTS = (3,5,7,11,13,15,19)
+
+    # GPIO to select the digits
     _DIGITS = (8,10,12,16)
+
+    # which of the 7 segments should be switched on to display the character
     _NUM = {
         '-':(1,1,1,1,1,1,0),
         ' ':(1,1,1,1,1,1,1),
@@ -24,6 +32,8 @@ class SegPlugin:
         '8':(0,0,0,0,0,0,0),
         '9':(0,0,0,0,1,0,0)
     }
+
+    # GPIO pin for the dots between the digits
     _DP = 21
 
     _LED = 22   # indicating outside temperature
@@ -31,7 +41,11 @@ class SegPlugin:
     _LED3 = 23  # warning indicator (outside warmer than inside, outside below 0 degrees)
 
     def __init__(self):
-        """ Initialisieren """
+        """
+        Initialize the class.
+        
+        @param      self  The object
+        """
         self._log = logging.getLogger(str(self))
         self._log.info("{0} init".format(self))
 
@@ -49,7 +63,13 @@ class SegPlugin:
         self._t2.start()
 
     def _initGPIO(self):
-        """ Raspberry GPIO initialisieren """
+        """
+        Initialize RPi GPIO.
+        
+        @param      self  The object
+        
+        @return     None
+        """
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
         GPIO.setup(self._DIGITS, GPIO.OUT)
@@ -67,24 +87,51 @@ class SegPlugin:
         GPIO.output(self._LED3, 1)
 
 
+
     def update(self, mod, tin, tout):
-        """ Werte aktualisieren """
+        """
+        @brief      Receive update of the temperature values.
+        
+        @param      self  The object
+        @param      mod   The modifier
+        @param      tin   The inside temperature value
+        @param      tout  The outside temperature value
+        
+        @return     None
+        """
+
         # print "update({0}, {1}, {2})".format(mod, tin, tout)
         self._tin = tin
         self._tout = tout
 
-        # schalte LED2 ein, wenn draußen wärmer als drinnen, oder wenn draußen kälter als 0°C
+        # schalte LED2 ein, wenn draußen wärmer als drinnen, oder wenn draußen
+        # kälter als 0°C
         if (tin < tout or tout < 0):
             GPIO.output(self._LED2, 1)
         else:
             GPIO.output(self._LED2, 0)
 
     def stop(self):
+        """
+        @brief      Stop the LED loop
+        
+        @param      self  The object
+        
+        @return     None
+        """
         self._run = False
         self._log.info("{0} stopped".format(self))
 
     def _runGPIO(self):
-        """ GPIO Schleife """
+        """
+        @brief      The LED loop. Loops over all digit and switches the
+                    repsective segments on to show the values safed in
+                    self._value.
+        
+        @param      self  The object
+        
+        @return     None
+        """
         while self._run:
             for digit in range(4):
                 GPIO.output(self._SEGMENTS, self._NUM[self._value[digit]])
@@ -97,27 +144,41 @@ class SegPlugin:
                 GPIO.output(self._DIGITS[digit], 0)
 
     def _alternate(self):
-        """ Wechsle zwischen innen und außen """
-        wait = 3.0
-        while self._run:
+        """
+        @brief      Alternate inside and outside temperature every 3 seconds
+        
+        @param      self  The object
+        
+        @return     None
+        """
 
+        wait = 3.0 # time to wait between switches
+    
+        while self._run:
 	    # inside
             t = int(time.strftime("%H"))
             self._value = self._formatValue(self._tin)
             GPIO.output(self._LED, 0)
-            if 6 <= t < 22:
+            if t in _HOURS:
                 GPIO.output(self._LED3, 1)
             time.sleep(wait)
 
 	    # outside
             self._value = self._formatValue(self._tout)
-            if 6 <= t < 22:
+            if t in _HOURS:
                 GPIO.output(self._LED, 1)
             GPIO.output(self._LED3, 0)
             time.sleep(wait)
 
     def _formatValue(self, value):
-        """ Wert für 7 Segment formatieren """
+        """
+        Format a temperature value for the 7 segment display.
+        
+        @param      self   The object
+        @param      value  The value
+        
+        @return     The formatted value.
+        """
         if abs(value) >= 100:
             #self._log.error("formatValue(): value was out of range: {0}".format(value))
             return "----"
@@ -128,14 +189,29 @@ class SegPlugin:
         newValue += '{:4.1f}'.format(abs(value))
         return newValue.replace(".", "")
 
+
     def __str__(self):
+        """
+        @brief      Returns a string representation of the object.
+        
+        @param      self  The object
+        
+        @return     String representation of the object.
+        """
         return self.__class__.__name__
 
     def __del__(self):
-        self._log.info("GPIO.cleanup(). save to restart now.")
+        self._log.info("GPIO.cleanup(). Save to restart now.")
         GPIO.cleanup()
 
+
 def main():
+    """
+    @brief      Main method to test the plugin.
+    
+    @return     None
+    """
+
     t = SegPlugin()
     t.update(None, 20.0, -100)
     n = -20
