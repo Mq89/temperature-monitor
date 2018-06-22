a471ffad711cc928ca4492369a8d4a13c1dda2de
Markus Heinrich <mh@0x25.net>
2018-06-22 23:05:00 +0200

    adjusted timing

--- plugins/display.py
+++ plugins/display.py
@@ -9,7 +9,7 @@ import logging
 class SegPlugin:
 
     # hours of day where the LED are allowed to be active
-    _HOURS = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+    _HOURS = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
 
     # GPIO pins for the 7 segmets
     _SEGMENTS = (3,5,7,11,13,15,19)
